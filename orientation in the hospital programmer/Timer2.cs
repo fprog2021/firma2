38242ab5b3becfe5e6c19fbbb82ce32d19843a6d
fprog2021 <87995038+fprog2021@users.noreply.github.com>
2021-07-30 17:46:23 +0700

    30.07.2021

---
+++ orientation in the hospital programmer/Timer2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class Timer2 : MonoBehaviour
+{
+    public float miliSecond;
+    public float second;
+    public float minute;
+    public Text text;
+    private void FixedUpdate()
+    {
+
+        miliSecond += 0.2f;
+        if (miliSecond >= 1)
+        {
+            second++;
+            miliSecond = 0;
+        }
+        if (second == 60)
+        {
+            minute++;
+            second = 0;
+        }
+
+        text.text = $"{minute} : {second}";
+    }
+}
+
