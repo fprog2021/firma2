c1b82321a4b18e04b1cc298e990f7c67a17c7286
fprog2021 <87995038+fprog2021@users.noreply.github.com>
2021-07-31 17:19:30 +0700

    Revert "Revert "30.07.2021""
    This reverts commit c75bbefdd3d4f73e2f0149b33bec8687e07a25a3.

---
+++ orientation in the hospital programmer/tab.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class tab : MonoBehaviour
+
+
+{
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Tab))
+			Debug.Log("Space key was pressed.");
+
+		if (Input.GetKeyUp(KeyCode.Tab))
+			Debug.Log("Space key was released.");
+	}
+}
